ba28becfa4d52c2ca8cb73ad180361e7d0ba8327
millennion-jin <Jadestern2@>
2022-04-18 15:36:09 +0900

    feat: GameService 단위에서 상황별 결과 도출값 테스트 생성

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -1,7 +1,10 @@
 package baseball.service;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import baseball.dto.GameResultDto;
 import org.junit.jupiter.api.Test;
 
 class GameServiceTest {
@@ -19,5 +22,85 @@ class GameServiceTest {
 
         assertThatThrownBy(() -> gameService.processGamePlay("95E"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 전부_틀렸을_경우() {
+        assertRandomNumberInRangeTest(
+            () -> {
+                GameService gameService = new GameService();
+                gameService.initGameService();
+
+                GameResultDto result = gameService.processGamePlay("162");
+                assertThat(result.getNumBall()).isEqualTo(0);
+                assertThat(result.getNumStrike()).isEqualTo(0);
+                assertThat(result.isFinished()).isFalse();
+            },
+            3, 5, 7
+        );
+    }
+
+    @Test
+    void 하나_맞췄을_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    GameService gameService = new GameService();
+                    gameService.initGameService();
+
+                    GameResultDto result = gameService.processGamePlay("312");
+                    assertThat(result.getNumBall()).isEqualTo(0);
+                    assertThat(result.getNumStrike()).isEqualTo(1);
+                    assertThat(result.isFinished()).isFalse();
+                },
+                3, 6, 9
+        );
+    }
+
+    @Test
+    void 하나_맞추고_2볼인_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    GameService gameService = new GameService();
+                    gameService.initGameService();
+
+                    GameResultDto result = gameService.processGamePlay("132");
+                    assertThat(result.getNumBall()).isEqualTo(2);
+                    assertThat(result.getNumStrike()).isEqualTo(1);
+                    assertThat(result.isFinished()).isFalse();
+                },
+                1, 2, 3
+        );
+    }
+
+    @Test
+    void 볼이_3개인_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    GameService gameService = new GameService();
+                    gameService.initGameService();
+
+                    GameResultDto result = gameService.processGamePlay("562");
+                    assertThat(result.getNumBall()).isEqualTo(3);
+                    assertThat(result.getNumStrike()).isEqualTo(0);
+                    assertThat(result.isFinished()).isFalse();
+                },
+                6, 2, 5
+        );
+    }
+
+    @Test
+    void 스트라이크가_3개인_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    GameService gameService = new GameService();
+                    gameService.initGameService();
+
+                    GameResultDto result = gameService.processGamePlay("478");
+                    assertThat(result.getNumBall()).isEqualTo(0);
+                    assertThat(result.getNumStrike()).isEqualTo(3);
+                    assertThat(result.isFinished()).isTrue();
+                },
+                4, 7, 8
+        );
     }
 }
