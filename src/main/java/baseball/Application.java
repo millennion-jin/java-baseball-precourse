b0f60fa1dbc069a3c556bbb0115a28d0c4c08dc1
millennion-jin <Jadestern2@>
2022-04-18 15:55:11 +0900

    feat: 콘솔 상에 게임 상태 출력 및 입력 값 확인 기능 및 게임 동작 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.ui.GameUI;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameUI gameUI = new GameUI();
+        gameUI.play();
     }
 }
--- src/main/java/baseball/ui/GameUI.java
+++ src/main/java/baseball/ui/GameUI.java
@@ -1,9 +1,53 @@
 package baseball.ui;
 
 import baseball.constants.GameMessageConstants;
+import baseball.constants.GameOptionConstants;
 import baseball.dto.GameResultDto;
+import baseball.service.GameService;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameUI {
+    private final GameService gameService;
+
+    public GameUI() {
+        gameService = new GameService();
+    }
+
+    public void play() {
+        do {
+            gameService.initGameService();
+            processGamePlay();
+        } while(checkContinue());
+
+
+    }
+
+    private void processGamePlay() {
+        while(true) {
+            printPleaseInputMsg();
+            String inputValue = readInputValue();
+
+            GameResultDto result = gameService.processGamePlay(inputValue);
+            printResult(result);
+
+            if(result.isFinished()) break;
+        }
+    }
+
+    private void printPleaseInputMsg() {
+        System.out.printf("%s : ", GameMessageConstants.MESSAGE_PLEASE_INPUT);
+    }
+
+    private String readInputValue() {
+        return Console.readLine();
+    }
+
+    private void printResult(GameResultDto dto) {
+        System.out.println(convertResultToMessage(dto));
+        if (dto.isFinished()) {
+            System.out.printf((GameMessageConstants.MESSAGE_FINISHED) + "%n", GameOptionConstants.OPTION_NUMBER_LENGTH);
+        }
+    }
 
     private String convertResultToMessage(GameResultDto dto) {
         StringBuilder builder = new StringBuilder();
@@ -41,6 +85,14 @@ public class GameUI {
         if (builder.length() != 0) {
             builder.append(" ");
         }
+    }
+
+    private boolean checkContinue() {
+        System.out.printf((GameMessageConstants.MESSAGE_CONTINUE) + "%n", GameOptionConstants.OPTION_CONTINUE_TEXT,
+                GameOptionConstants.OPTION_FINISH_TEXT);
+
+        String inputValue = readInputValue();
+        return gameService.checkContinueGame(inputValue);
     }
 
 }
