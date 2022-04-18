4067d4340f11b6bb7de673cb4b70c22533ac056b
millennion-jin <Jadestern2@>
2022-04-18 15:06:23 +0900

    feat: Constants 파일 생성

---
+++ src/main/java/baseball/constants/GameOptionConstants.java
@@ -0,0 +1,7 @@
+package baseball.constants;
+
+public class GameOptionConstants {
+    public static final int OPTION_NUMBER_LENGTH = 3;
+    public static final int OPTION_NUMBER_RANGE_START = 1;
+    public static final int OPTION_NUMBER_RANGE_END = 9;
+}
